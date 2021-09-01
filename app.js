4a591147aefb0911e07bac0bc0fc53fce7fe2ab7
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-09-01 15:52:53 +0530

    file upload

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const bodyParser = require("body-parser");
+var formidable = require("formidable");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // const encodeVideo = require("ffmpeg-hls");
 
@@ -11,18 +16,34 @@ const { v4: uuidv4 } = require("uuid");
 
 // app.get("/", (req, res) => res.send("Video Encoding"));
 
-// app.get("/download", (req, res) => {
-//   res.download(
-//     __dirname + "\\" + "b5f56320-a039-47c6-8c39-16f6a56b8598.key",
-//     (err) => {
-//       if (err) console.log(err);
-//     }
-//   );
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "\\index.html");
+});
 
-// app.listen(8080, () => {
-//   console.log("server is running");
-// });
+app.get("/download", (req, res) => {
+  res.download(
+    __dirname + "\\" + "b5f56320-a039-47c6-8c39-16f6a56b8598.key",
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+});
+
+app.listen(8080, () => {
+  console.log("server is running");
+});
+
+app.post("/upload", (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.uploadDir = "./video";
+  form.keepExtensions = true;
+  form.parse(req, async (err, fields, files) => {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write("received upload:\n\n");
+    console.log(files.video.path);
+    // var stream = fs.createWriteStream(__dirname + "/video", +files.video.path);
+  });
+});
 
 // const keyId = uuidv4();
 
