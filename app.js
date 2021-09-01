dd9f410ded17b24aee86c8c16b0a61e7a0f136d3
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-09-02 00:24:21 +0530

    succefully serverd key over the network

--- app.js
+++ app.js
@@ -6,15 +6,38 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
 var formidable = require("formidable");
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// const encodeVideo = require("ffmpeg-hls");
-
-// encodeVideo();
+const encodeVideo = require("ffmpeg-hls");
 
 // app.get("/", (req, res) => res.send("Video Encoding"));
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "\\index.html");
@@ -22,7 +45,8 @@ app.get("/", (req, res) => {
 
 app.get("/download", (req, res) => {
   res.download(
-    __dirname + "\\" + "b5f56320-a039-47c6-8c39-16f6a56b8598.key",
+    __dirname +
+      "\\encryptedVideos\\3d4bb0f5-4715-42d0-a590-546dc550882d\\3d4bb0f5-4715-42d0-a590-546dc550882d.key",
     (err) => {
       if (err) console.log(err);
     }
@@ -35,12 +59,28 @@ app.listen(8080, () => {
 
 app.post("/upload", (req, res) => {
   var form = new formidable.IncomingForm();
-  form.uploadDir = "./video";
+  var id = uuidv4();
+  if (!fs.existsSync("encryptedVideos")) {
+    fs.mkdirSync("encryptedVideos");
+    fs.mkdirSync(`encryptedVideos/${id}`);
+  } else {
+    fs.mkdirSync(`encryptedVideos/${id}`);
+  }
+
+  form.uploadDir = `./encryptedVideos/${id}`;
+
   form.keepExtensions = true;
+
   form.parse(req, async (err, fields, files) => {
     res.writeHead(200, { "content-type": "text/plain" });
     res.write("received upload:\n\n");
-    console.log(files.video.path);
+    // console.log(files.video.path.slice(53));
+    encodeVideo(
+      id,
+      files.video.path.slice(53),
+      "http://localhost:8080/download"
+    );
+
     // var stream = fs.createWriteStream(__dirname + "/video", +files.video.path);
   });
 });
